9d97ddbb38e40dd206a9d04b38caf3e03a5ccce7
lcfz86 <zerolcfz86@gmail.com>
2021-06-13 19:26:51 +0800

    Add OOP Example 2

---
+++ contact_book.rb
@@ -0,0 +1,24 @@
+class Contact
+  attr_accessor :name, :phone_num, :email
+
+  def initialize(name, phone_num, email)
+    @name = name
+    @phone_num = phone_num
+    @email = email
+  end
+end
+
+contact = []
+contact.push Contact.new('Joseph', '016-888 0952', 'joseph@example.com')
+contact.push Contact.new('Fredrick', '016-238 0909', 'fredrick@example.com')
+contact.push Contact.new('Sanderson', '016-663 0343 ', 'sanderson@example.com')
+contact.push Contact.new('Martin', '019-388 2182 ', 'martin@example.com')
+contact.push Contact.new('Ernest', '012-501 5721 ', 'ernest@example.com')
+
+contact.each do |contacts|
+  puts "-" * 40
+  puts "Name: #{contacts.name}"
+  puts "Phone Number: #{contacts.phone_num}"
+  puts "Email Address: #{contacts.email}"
+  puts "=" * 40
+end
