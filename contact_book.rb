02a1b4a8b775d4cfdd7a1710fdec2470bafb4cf1
lcfz86 <zerolcfz86@gmail.com>
2021-06-13 19:47:57 +0800

    Add OOP Example 2

--- contact_book.rb
+++ contact_book.rb
@@ -11,7 +11,7 @@ end
 contact = []
 contact.push Person.new('Joseph', '016-888 0952', 'joseph@example.com')
 contact.push Person.new('Fredrick', '016-238 0909', 'fredrick@example.com')
-contact.push Person.new('Sanderson', '016-663 0343 ', 'sanderson@example.com')
+contact.push Person.new('Anderson', '016-663 0343 ', 'Anderson@example.com')
 contact.push Person.new('Martin', '019-388 2182 ', 'martin@example.com')
 contact.push Person.new('Ernest', '012-501 5721 ', 'ernest@example.com')
 
