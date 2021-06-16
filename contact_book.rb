76352711babbb3c1a5ea9b8a7e529aa0ef19ae72
lcfz86 <zerolcfz86@gmail.com>
2021-06-16 20:27:16 +0800

    Add OOP Example 3

--- contact_book.rb
+++ contact_book.rb
@@ -6,19 +6,28 @@ class Person
     @phone_num = phone_num
     @email = email
   end
+
+  def contact_card
+    puts "-" * 40
+    name = "Name: #{@name}\n"
+    phone_num = "Phone Number: #{@phone_num}\n"
+    email = "Email Address: #{@email}\n"
+    line = "=" * 40
+    name.concat(phone_num, email, line)
+  end
 end
 
-contact = []
-contact.push Person.new('Joseph', '016-888 0952', 'joseph@example.com')
-contact.push Person.new('Fredrick', '016-238 0909', 'fredrick@example.com')
-contact.push Person.new('Anderson', '016-663 0343 ', 'Anderson@example.com')
-contact.push Person.new('Martin', '019-388 2182 ', 'martin@example.com')
-contact.push Person.new('Ernest', '012-501 5721 ', 'ernest@example.com')
+#contact = []
+#contact.push Person.new('Joseph', '016-888 0952', 'joseph@example.com')
+#contact.push Person.new('Fredrick', '016-238 0909', 'fredrick@example.com')
+#contact.push Person.new('Anderson', '016-663 0343 ', 'Anderson@example.com')
+#contact.push Person.new('Martin', '019-388 2182 ', 'martin@example.com')
+#contact.push Person.new('Ernest', '012-501 5721 ', 'ernest@example.com')
 
-contact.each do |contacts|
-  puts "-" * 40
-  puts "Name: #{contacts.name}"
-  puts "Phone Number: #{contacts.phone_num}"
-  puts "Email Address: #{contacts.email}"
-  puts "=" * 40
-end
+#contact.each do |contacts|
+#  puts "-" * 40
+#  puts "Name: #{contacts.name}"
+#  puts "Phone Number: #{contacts.phone_num}"
+#  puts "Email Address: #{contacts.email}"
+#  puts "=" * 40
+#end
